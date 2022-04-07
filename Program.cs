d12f8b6ef5d960ce98ae4590f288081f08b5d161
satyamwathrey <swathrey1@gmail.com>
2022-04-08 00:05:13 +0530

    Ability to compute Employee wage for multiple companies

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace EmployeeWage
 {
@@ -6,49 +11,18 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            const int FullTime = 1;
-            const int PartTime = 2;
-            const int WorkingDays = 20;
-            const int empRatePerHr = 20;
-            const int MaxHrsPerMonth = 100;
-            int empHrs = 0;
-            int empPayment = 0;
-            int TotalPayment = 0;
-            int TotalEmpHrs = 0;
-            int day = 1;
-
-            Random random = new Random();
-            
-            while(day <= WorkingDays && TotalEmpHrs <= MaxHrsPerMonth)
-            {
-              int empInput = random.Next(0, 3);
-
-                switch(empInput)
-                {
-                    case FullTime:
-                    empHrs = 8;
-                        break;
-
-                    case PartTime:
-                    empHrs = 4;
-                        break;
-
-                    default:
-                    empHrs = 0;
-                        break;
-                }
-          
-                empPayment = empRatePerHr * empHrs;
-                Console.WriteLine("Daily Wage for day {0} is: {1} for {2} Hrs", day, empPayment, empHrs);
-                TotalPayment += empPayment;
-                TotalEmpHrs += empHrs;
-                day++;
-            
-            }
-
-            Console.WriteLine("Total Days {0}, Total Working hours: {1}", (day-1), TotalEmpHrs);
-            Console.WriteLine("Total Employee Wage is: " + TotalPayment);
+            Console.WriteLine("Welcome to Empolyee Wage Computation Program\n");
+            EmpAttendance.GetEmpAttendance();
+            EmployeeWage.GetEmpWage();
+            PartTimeEmployee.PartTimeWage();
+            SwitchCase.DailyEmpWage();
+            MonthWages.MonthlyEmpWage();
+            WageTillCondition.GetWageTillCondition();
+            RefactorWithClassMethod.RefactorGetMonthlyEmpWage();
+            EmpWageForMultipleCompanies.ComputeEmpWage("Google", 20, 2, 10);
+            EmpWageForMultipleCompanies.ComputeEmpWage("Microsoft", 10, 4, 20);
+            EmpWageForMultipleCompanies.ComputeEmpWage("Deloitte", 15, 3, 15);
             Console.ReadLine();
-        }
+        }    
     }
 }
